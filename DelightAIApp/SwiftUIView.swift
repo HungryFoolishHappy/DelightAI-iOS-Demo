232200487b518961a3144361ccceecc656c88583
Jack Chau <jack@fyinnovate.com>
2024-09-03 19:49:52 +0800

    add slash screen code

--- DelightAIApp/SwiftUIView.swift
+++ DelightAIApp/SwiftUIView.swift
@@ -11,32 +11,49 @@ struct SwiftUIView: View {
     @State private var webhookId: String = "a4b404d4-5f9a-4617-a5a4-8ea338352eb4"
     @State private var userId: String = "Wi-iOS-9937-491d-aefd-02e6b89ff0aa"
     @State private var username: String = "Wi-iOS-9937-491d-aefd-02e6b89ff0aa"
+    @State var isActive: Bool = false
     
     var body: some View {
         NavigationStack{
-            Image("DelightLogo", label: Text("DelightLogo"))            
-            Spacer()
-            Text("Delight Demo").font(.title2)
-            
-            Form {
-                HStack{
-                    Text("Webhook Id").frame(width: 100, alignment: .leading)
-                    TextField("Required", text: $webhookId)
+            if self.isActive {
+                Image("DelightLogo", label: Text("DelightLogo"))
+                Spacer()
+                Text("Delight Demo").font(.title2)
+                
+                Form {
+                    HStack{
+                        Text("Webhook Id").frame(width: 100, alignment: .leading)
+                        TextField("Required", text: $webhookId)
+                    }
+                    
+                    HStack{
+                        Text("Username").frame(width: 100, alignment: .leading)
+                        TextField("Required", text: $username)
+                    }
+                    
+                    HStack{
+                        Text("User Id").frame(width: 100, alignment: .leading)
+                        TextField("Required", text: $userId)
+                    }
                 }
                 
-                HStack{
-                    Text("Username").frame(width: 100, alignment: .leading)
-                    TextField("Required", text: $username)
+                NavigationLink(destination: ContentView(webhookId: webhookId, userId: userId, username: username)) {
+                    Text("Submit")
                 }
-                
-                HStack{
-                    Text("User Id").frame(width: 100, alignment: .leading)
-                    TextField("Required", text: $userId)
-                }
+            } else {
+                Image("DelightScreen")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity)
+                    .ignoresSafeArea(edges: .all)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 100)
+                    .clipped()
             }
-            
-            NavigationLink(destination: ContentView(webhookId: webhookId, userId: userId, username: username)) {
-                Text("Submit")
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation {
+                    self.isActive = true
+                }
             }
         }
     }
